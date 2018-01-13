e41af7726a1b7b42f500c4ef5f4607f05d6709cb
Michael Masterson <michaelmasterson@Michaels-MacBook-Pro.local>
2018-01-12 16:58:51 -0800

    Medium Api displaying

--- server.js
+++ server.js
@@ -13,11 +13,24 @@ app.get('/api/github', (req, res) => {
       'Authorization': `token ${process.env.OAUTH}`
     }
   })
-  .then((data) => {
-    return data.json()
-  })
+  .then(data => data.json())
   .then(data => res.json(data))
 })
+
+app.get('/api/medium', (req, res) => {
+
+  const url = 'http://rss2json.com/api.json?rss_url=https://medium.com/feed/queers-in-tech'
+
+  fetch(url)
+  .then(data => data.json())
+  .then(data => res.json(data))
+})
+
+// GET https://api.medium.com/v1/users/{{userId}}/publications
+
+// GET https://api.medium.com/v1/publications/{{publicationId}}/contributors
+
+// GET https://medium.com/m/oauth/authorize?client_id=6e2ef59844b4&scope=basicProfile,listPublications&state=bunnys&response_type=code&redirect_uri=http://queersintech.com
 
 const port = 5000
 
